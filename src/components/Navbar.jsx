c8aa5667e3d62ee8277e118f4ef401851f61f7d0
Yamanadm <adsamu44@gmail.com>
2022-11-16 13:19:23 +0300

    register state completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,75 +1,78 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/icons/avatar.png";
 
 const Navbar = () => {
+  const currentUser = { displayName: "adem yaman" };
   return (
     <>
-      <nav className="-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg fixed-top">
+      <nav
+        className="w-full flex  items-center justify-around
+       py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg fixed-top"
+      >
         <div className="container-fluid w-full flex  items-center justify-between px-6">
-          <div
-            className="collapse navbar-collapse flex-grow items-center"
-            id="navbarSupportedContent1"
-          >
-            <a className="text-xl text-white pr-2 font-semibold" href="#">
-              MOVİE APP
-            </a>
-          </div>
-          {/* Collapsible wrapper */}
-          {/* Right elements */}
-          <div className="flex items-center relative">
-            {/* Icon */}
+          <Link className="text-2xl text-white pr-2 font-semibold" to="/">
+            MOVİE APP
+          </Link>
+        </div>
 
-            <div className="dropdown relative">
-              <a
-                className="dropdown-toggle flex items-center hidden-arrow"
-                href="#"
-                id="dropdownMenuButton2"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <img
-                  src="https://mdbootstrap.com/img/new/avatars/2.jpg"
-                  className="rounded-full"
-                  style={{ height: 25, width: 25 }}
-                  alt=""
-                  loading="lazy"
-                />
-              </a>
-              <ul
-                className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0"
-                aria-labelledby="dropdownMenuButton2"
-              >
-                <li>
-                  <a
-                    className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                    href="#"
-                  >
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                    href="#"
-                  >
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
-                    href="#"
-                  >
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </div>
+        <div className="flex items-center relative">
+          {/* Icon */}
+
+          {currentUser && (
+            <h5 className="capitalize mr-2">{currentUser?.displayName}</h5>
+          )}
+
+          <div className="dropdown  px-6">
+            <span
+              className="dropdown-toggle flex items-center hidden-arrow"
+              href="#"
+              id="dropdownMenuButton2"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src={currentUser?.photoURL || avatar}
+                className="rounded-full "
+                style={{ height: 25, width: 25 }}
+                alt=""
+                loading="lazy"
+              />
+            </span>
+            <ul
+              className="dropdown-menu min-w-max absolute  bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0"
+              aria-labelledby="dropdownMenuButton2"
+            >
+              <li>
+                <Link
+                  className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  to="/register"
+                >
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  to="/login"
+                >
+                  Login
+                </Link>
+              </li>
+              <li>
+                <span
+                  className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  role="button"
+                >
+                  Logout
+                </span>
+              </li>
+            </ul>
           </div>
-          {/* Right elements */}
         </div>
       </nav>
-      <div className="h-[52px]"></div>
+      <div className="h-[70px]"></div>
     </>
   );
 };
